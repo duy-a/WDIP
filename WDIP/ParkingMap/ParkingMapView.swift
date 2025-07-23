360e38d1aa0b8b02115f5a60b81baa9fb2953547
Duy Anh Ngac <ngac.duyanh@icloud.com>
2025-07-23 15:44:39 +0700

    add animation and states for a saved and non saved parking spot

--- WDIP/ParkingMap/ParkingMapView.swift
+++ WDIP/ParkingMap/ParkingMapView.swift
@@ -13,44 +13,116 @@ struct ParkingMapView: View {
 
     @State private var userPosition: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
 
-    var body: some View {
-        ZStack(alignment: .bottom) {
-            Map(position: $userPosition) {
-                UserAnnotation()
-                
-                Annotation("Apple Visitor Center", coordinate: Constants.appleVisitorCetnerCoordinates, anchor: .center) {
-                    Image(systemName: "car.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundStyle(.white)
-                        .frame(width: 20, height: 20)
-                        .padding(8)
-                        .background(.indigo.gradient, in: .circle)
-                        .glassEffect()
-                }
-            }
-            .overlay {
-                Image(systemName: "mappin")
-                    .font(.largeTitle)
-                    .foregroundColor(.red)
-                    .offset(y: -15)
-            }
-            .onAppear {
-                locationManager.requestAuthorization()
-            }
+    @State private var isParkingSpotSaved: Bool = false
+    @Namespace private var namespace
 
-            HStack {
-                Button {
-                    //
-                } label: {
-                    Label("Park Here", systemImage: "car")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
+    @State private var toastOpacity: Double = 0
+
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                Map(position: $userPosition) {
+                    UserAnnotation()
+
+                    if isParkingSpotSaved {
+                        Annotation("Apple Visitor Center", coordinate: Constants.appleVisitorCetnerCoordinates, anchor: .center) {
+                            Image(systemName: "car.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.white)
+                                .frame(width: 20, height: 20)
+                                .padding(8)
+                                .background(.indigo.gradient, in: .circle)
+                                .glassEffect()
+                        }
+                    }
                 }
-                .buttonStyle(.glassProminent)
-                .controlSize(.extraLarge)
+                .overlay {
+                    if !isParkingSpotSaved {
+                        Image(systemName: "mappin")
+                            .font(.largeTitle)
+                            .foregroundColor(.red)
+                            .offset(y: -15)
+                    }
+                }
+                .onAppear {
+                    locationManager.requestAuthorization()
+                }
+
+                VStack {
+                    Text("Parking Spot Saved")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .glassEffect()
+                        .padding(.horizontal)
+                        .opacity(toastOpacity)
+
+                    Spacer()
+
+                    GlassEffectContainer(spacing: 10) {
+                        HStack(spacing: 10) {
+                            if !isParkingSpotSaved {
+                                Button {
+                                    withAnimation {
+                                        isParkingSpotSaved.toggle()
+                                    }
+                                    
+                                    Task {
+                                        withAnimation(.bouncy) {
+                                            toastOpacity = 1.0
+                                        }
+
+                                        try? await Task.sleep(for: .seconds(1), tolerance: .milliseconds(50))
+
+                                        withAnimation(.easeOut(duration: 2.0)) {
+                                            toastOpacity = 0.0
+                                        }
+                                    }
+                                } label: {
+                                    Label("Park Here", systemImage: "car")
+                                        .font(.title2)
+                                        .frame(maxWidth: .infinity)
+                                        .glassEffectID("parked-directions", in: namespace)
+                                }
+                                .buttonStyle(.glassProminent)
+                                .controlSize(.extraLarge)
+                            }
+
+                            if isParkingSpotSaved {
+                                Button {
+                                    withAnimation {
+                                        isParkingSpotSaved.toggle()
+                                    }
+                                } label: {
+                                    Label("Get Directions", systemImage: "arrow.turn.left.up")
+                                        .font(.title2)
+                                        .frame(maxWidth: .infinity)
+                                        .glassEffectID("parked-directions", in: namespace)
+                                }
+                                .buttonStyle(.glassProminent)
+                                .controlSize(.extraLarge)
+
+                                Button {
+                                    //
+                                } label: {
+                                    Label("Parking Spot Details", systemImage: "info")
+                                        .font(.title2)
+                                        .labelStyle(.iconOnly)
+                                        .glassEffectID("info", in: namespace)
+                                }
+                                .buttonStyle(.glass)
+                                .controlSize(.extraLarge)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
             }
-            .padding(.horizontal)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Label("Back", systemImage: "chevron.left")
+                }
+            }
         }
     }
 
